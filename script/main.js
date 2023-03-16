5244d73d167ec7d8f1413190c47c6e00e09fe603
181850036 <783473919@qq.com>
2023-03-16 17:23:36 +0800

    modify animation

--- script/main.js
+++ script/main.js
@@ -28,15 +28,15 @@ const fetchData = () => {
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
-  const hbd = document.getElementsByClassName("wish-hbd")[0];
+  // const hbd = document.getElementsByClassName("wish-hbd")[0];
 
   textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
     .split("")
     .join("</span><span>")}</span`;
 
-  hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
+  // hbd.innerHTML = `<span>${hbd.innerHTML
+  //   .split("")
+  //   .join("</span><span>")}</span`;
 
   const ideaTextTrans = {
     opacity: 0,
@@ -195,66 +195,7 @@ const animationTimeline = () => {
       0.2,
       "+=1"
     )
-    .staggerFromTo(
-      ".baloons img",
-      2.5,
-      {
-        opacity: 0.9,
-        y: 1400
-      },
-      {
-        opacity: 1,
-        y: -1000
-      },
-      0.2
-    )
-    .from(
-      ".lydia-dp",
-      0.5,
-      {
-        scale: 3.5,
-        opacity: 0,
-        x: 25,
-        y: -25,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
-      rotation: -180,
-      opacity: 0
-    })
-    .staggerFrom(
-      ".wish-hbd span",
-      0.7,
-      {
-        opacity: 0,
-        y: -50,
-        // scale: 0.3,
-        rotation: 150,
-        skewX: "30deg",
-        ease: Elastic.easeOut.config(1, 0.5)
-      },
-      0.1
-    )
-    .staggerFromTo(
-      ".wish-hbd span",
-      0.7,
-      {
-        scale: 1.4,
-        rotationY: 150
-      },
-      {
-        scale: 1,
-        rotationY: 0,
-        color: "#ff69b4",
-        ease: Expo.easeOut
-      },
-      0.1,
-      "party"
-    )
+
     .from(
       ".wish h5",
       0.5,
@@ -265,26 +206,10 @@ const animationTimeline = () => {
       },
       "party"
     )
-    .staggerTo(
-      ".eight svg",
-      1.5,
-      {
-        visibility: "visible",
-        opacity: 0,
-        scale: 80,
-        repeat: 3,
-        repeatDelay: 1.4
-      },
-      0.3
-    )
-    .to(".six", 0.5, {
-      opacity: 0,
-      y: 30,
-      zIndex: "-1"
-    })
+
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
-      ".last-smile",
+      ".gift",
       0.5,
       {
         rotation: 90
@@ -301,6 +226,5 @@ const animationTimeline = () => {
     tl.restart();
   });
 };
-
 // Run fetch and animation in sequence
 fetchData();
